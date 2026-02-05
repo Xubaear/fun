d34ef31889a622e5ed5177c4c88dd1bae1613d5b
Md Zubaear Hasan <zubaear.hasan7@gmail.com>
2026-02-05 13:15:28 +0600

    yo

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,129 +1,135 @@
 import React from 'react';
-
+import img from '../assets/vs.png'
 const Home = () => {
     return (
        
         <div className="relative min-h-screen w-full bg-black overflow-x-hidden">
             
            
-            <div className="absolute inset-0 h-full w-full pointer-events-none bg-[linear-gradient(to_right,#ffffff0a_1px,transparent_1px),linear-gradient(to_bottom,#ffffff0a_1px,transparent_1px)] bg-size-[40px_40px]"></div>
+            <div className="absolute inset-0 h-full w-full pointer-events-none bg-[linear-gradient(to_right,#ffffff0a_1px,transparent_1px),linear-gradient(to_bottom,#ffffff0a_1px,transparent_1px)] bg-[length:40px_40px] md:bg-[length:40px_40px] bg-[length:20px_20px]"></div>
             
             
-            <div className="relative z-10 p-10 text-white">
-                <h1 className="text-4xl font-bold ">আসন্ন <span className='text-red-500'>১৩তম জাতীয় সংসদ নির্বাচন ২০২৫</span> উপলক্ষে <br /> ম্যাছের সকল বর্ডারের ভোটের প্রেডিকশন !</h1>
+            <div className="relative z-10 p-4 md:p-10 text-white">
+                <h1 className="text-2xl md:text-4xl font-bold text-center leading-tight">আসন্ন <span className='text-red-500'>১৩তম জাতীয় সংসদ নির্বাচন ২০২৫</span> উপলক্ষে <br />ম্যাছের সকল বর্ডারের ভোটের প্রেডিকশন !</h1>
+
+                <div className='mt-10 md:mt-20 flex justify-center items-center' >
+                    <img src={img} className='h-auto w-full max-w-md md:max-w-none md:h-100 md:w-180 rounded-2xl' alt="" />
+                </div>
                 
 
-            <div className='mt-20'>
-                <h1 className='my-10 text-2xl font-bold bg-cyan-900 p-4 rounded-2xl'>পীরে কামেল জামাতের একনিষ্ঠ রাজু ছাত্রাবাসের ফেসবুকে উল্টা পাল্টা কমেন্টকারী গুপ্ত কর্মী পাটগ্রামের ভবিষ্যত জজ 
-                    <span className='text-3xl text-red-600'> নাজমুল হাদি রোমান</span>
+            <div className='mt-10 md:mt-20'>
+                <h1 className='my-6 md:my-10 text-lg md:text-2xl font-bold bg-cyan-900 p-3 md:p-4 rounded-2xl'>পীরে কামেল জামাতের একনিষ্ঠ রাজু ছাত্রাবাসের ফেসবুকে উল্টা পাল্টা কমেন্টকারী গুপ্ত কর্মী পাটগ্রামের ভবিষ্যত জজ 
+                    <span className='text-xl md:text-3xl text-red-600'> নাজমুল হাদি রোমান</span>
                      </h1>
-                <div className='flex justify-evenly items-center font-bold '>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>বিএনপি <br />
-                        <p className='mt-2'>১৫৫ থেকে ১৬৫</p>
+                <div className='flex flex-col md:flex-row justify-evenly items-center font-bold gap-4 md:gap-0'>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>বিএনপি <br />
+                        <span className='mt-2 block'>১৫৫ থেকে ১৬৫</span>
                     </p>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>জামাত<br />
-                        <p className='mt-2'>১১৫ থেকে ১২৫</p>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>জামাত<br />
+                        <span className='mt-2 block'>১১৫ থেকে ১২৫</span>
                     </p>
                 </div>
             </div>
 
 
-            <div className='mt-20'>
-                <h1 className='my-10 text-2xl font-bold bg-cyan-900 p-4 rounded-2xl'>বিএনপির একনিষ্ঠ তারেক জিয়াকে গাইলানো কর্মী শেখ মুজিব থুক্কু 
-                    <span className='text-3xl text-red-600'> শেখ রিফাত</span>
+            <div className='mt-10 md:mt-20'>
+                <h1 className='my-6 md:my-10 text-lg md:text-2xl font-bold bg-cyan-900 p-3 md:p-4 rounded-2xl'>বিএনপির একনিষ্ঠ তারেক জিয়াকে গাইলানো কর্মী শেখ মুজিব থুক্কু 
+                    <span className='text-xl md:text-3xl text-red-600'> শেখ রিফাত</span>
                 </h1>
-                <div className='flex justify-evenly items-center font-bold '>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>বিএনপি <br />
-                        <p className='mt-2'>১৮০ থেকে ২০০</p>
+                <div className='flex flex-col md:flex-row justify-evenly items-center font-bold gap-4 md:gap-0'>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>বিএনপি <br />
+                        <span className='mt-2 block'>১৮০ থেকে ২০০</span>
                     </p>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>জামাত<br />
-                        <p className='mt-2'>৬০ থেকে ৮০</p>
-                    </p>
-                </div>
-            </div>
-
-
-            <div className='mt-20'>
-                <h1 className='my-10 text-2xl font-bold bg-cyan-900 p-4 rounded-2xl'>জামাতের হিন্দু শাখার রাজু ছাত্রাবাসের সভাপতি 
-                    <span className='text-3xl text-red-600'> আনন্দ কুমার রয়</span> </h1>
-                <div className='flex justify-evenly items-center font-bold '>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>বিএনপি <br />
-                        <p className='mt-2'>১২০ থেকে ১৩০</p>
-                    </p>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>জামাত<br />
-                        <p className='mt-2'>১৬০ থেকে ১৭০</p>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>জামাত<br />
+                        <span className='mt-2 block'>৬০ থেকে ৮০</span>
                     </p>
                 </div>
             </div>
 
 
-            <div className='mt-20'>
-                <h1 className='my-10 text-2xl font-bold bg-cyan-900 p-4 rounded-2xl'>বিশিষ্ট টিকটকার নিজেও জানে না কি দিয়া কি বলে 
-                    <span className='text-3xl text-red-600'> মো: নাঈম</span>
+            <div className='mt-10 md:mt-20'>
+                <h1 className='my-6 md:my-10 text-lg md:text-2xl font-bold bg-cyan-900 p-3 md:p-4 rounded-2xl'>জামাতের হিন্দু শাখার রাজু ছাত্রাবাসের সভাপতি 
+                    <span className='text-xl md:text-3xl text-red-600'> আনন্দ কুমার রয়</span> </h1>
+                <div className='flex flex-col md:flex-row justify-evenly items-center font-bold gap-4 md:gap-0'>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>বিএনপি <br />
+                        <span className='mt-2 block'>১২০ থেকে ১৩০</span>
+                    </p>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>জামাত<br />
+                        <span className='mt-2 block'>১৬০ থেকে ১৭০</span>
+                    </p>
+                </div>
+            </div>
+
+
+            <div className='mt-10 md:mt-20'>
+                <h1 className='my-6 md:my-10 text-lg md:text-2xl font-bold bg-cyan-900 p-3 md:p-4 rounded-2xl'>বিশিষ্ট টিকটকার নিজেও জানে না কি দিয়া কি বলে 
+                    <span className='text-xl md:text-3xl text-red-600'> মো: নাঈম</span>
                 </h1>
-                <div className='flex justify-evenly items-center font-bold '>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>বিএনপি <br />
-                        <p className='mt-2'>৯০ থেকে ১০০</p>
+                <div className='flex flex-col md:flex-row justify-evenly items-center font-bold gap-4 md:gap-0'>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>বিএনপি <br />
+                        <span className='mt-2 block'>৯০ থেকে ১০০</span>
                     </p>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>জামাত<br />
-                        <p className='mt-2'>২০০ থেকে ২১০</p>
-                    </p>
-                </div>
-            </div>
-
-
-            <div className='mt-20'>
-                <h1 className='my-10 text-2xl font-bold bg-cyan-900 p-4 rounded-2xl'>ম্যাছ ম্যানেজার <span className='text-3xl text-red-600'> জারিফ</span> </h1>
-                <div className='flex justify-evenly items-center font-bold '>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>বিএনপি <br />
-                        <p className='mt-2'>১৫৫ থেকে ১৬৫</p>
-                    </p>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>জামাত<br />
-                        <p className='mt-2'>১১৫ থেকে ১২৫</p>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>জামাত<br />
+                        <span className='mt-2 block'>২০০ থেকে ২১০</span>
                     </p>
                 </div>
             </div>
 
 
-            <div className='mt-20'>
-                <h1 className='my-10 text-2xl font-bold bg-cyan-900 p-4 rounded-2xl'>ম্যাছের সহকারী ম্যানেজার <span className='text-3xl text-red-600'> আফ্রিদি</span></h1>
-                <div className='flex justify-evenly items-center font-bold '>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>বিএনপি <br />
-                        <p className='mt-2'>২২০ থেকে ২৩০</p>
+            <div className='mt-10 md:mt-20'>
+                <h1 className='my-6 md:my-10 text-lg md:text-2xl font-bold bg-cyan-900 p-3 md:p-4 rounded-2xl'>ম্যাছ ম্যানেজার <span className='text-xl md:text-3xl text-red-600'> জারিফ</span> </h1>
+                <div className='flex flex-col md:flex-row justify-evenly items-center font-bold gap-4 md:gap-0'>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>বিএনপি <br />
+                        <span className='mt-2 block'>১৫৫ থেকে ১৬৫</span>
                     </p>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>জামাত<br />
-                        <p className='mt-2'>৫০ থেকে ৬০</p>
-                    </p>
-                </div>
-            </div>
-
-
-            <div className='mt-20'>
-                <h1 className='my-10 text-2xl font-bold bg-cyan-900 p-4 rounded-2xl'>মন থেকে একদল করে কিন্তু পীরে কামালের ভয়ে বলতে পারে না <span className='text-3xl text-red-600'> সৈকত</span></h1>
-                <div className='flex justify-evenly items-center font-bold '>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>বিএনপি <br />
-                        <p className='mt-2'>১৯০ থেকে ২০০</p>
-                    </p>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>জামাত<br />
-                        <p className='mt-2'>৭০ থেকে ৮০</p>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>জামাত<br />
+                        <span className='mt-2 block'>১১৫ থেকে ১২৫</span>
                     </p>
                 </div>
             </div>
 
 
-            <div className='mt-20'>
-                <h1 className='my-10 text-2xl font-bold bg-cyan-900 p-4 rounded-2xl'>ম্যাসের একমাত্র নিরপেক্ষ মানুষ <span className='text-3xl text-red-600'> জুবায়ের</span></h1>
-                <div className='flex justify-evenly items-center font-bold '>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>বিএনপি <br />
-                        <p className='mt-2'>১৮০ থেকে ১২০</p>
+            <div className='mt-10 md:mt-20'>
+                <h1 className='my-6 md:my-10 text-lg md:text-2xl font-bold bg-cyan-900 p-3 md:p-4 rounded-2xl'>ম্যাছের সহকারী ম্যানেজার <span className='text-xl md:text-3xl text-red-600'> আফ্রিদি</span></h1>
+                <div className='flex flex-col md:flex-row justify-evenly items-center font-bold gap-4 md:gap-0'>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>বিএনপি <br />
+                        <span className='mt-2 block'>২২০ থেকে ২৩০</span>
                     </p>
-                    <p className='text-3xl bg-[#6c6867] items-center p-4 rounded-2xl'>জামাত<br />
-                        <p className='mt-2'>৮০ থেকে ৯০</p>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>জামাত<br />
+                        <span className='mt-2 block'>৫০ থেকে ৬০</span>
+                    </p>
+                </div>
+            </div>
+
+
+            <div className='mt-10 md:mt-20'>
+                <h1 className='my-6 md:my-10 text-lg md:text-2xl font-bold bg-cyan-900 p-3 md:p-4 rounded-2xl'>মন থেকে একদল করে কিন্তু পীরে কামালের ভয়ে বলতে পারে না <span className='text-xl md:text-3xl text-red-600'> সৈকত</span></h1>
+                <div className='flex flex-col md:flex-row justify-evenly items-center font-bold gap-4 md:gap-0'>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>বিএনপি <br />
+                        <span className='mt-2 block'>১৯০ থেকে ২০০</span>
+                    </p>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>জামাত<br />
+                        <span className='mt-2 block'>৭০ থেকে ৮০</span>
+                    </p>
+                </div>
+            </div>
+
+
+            <div className='mt-10 md:mt-20 mb-10'>
+                <h1 className='my-6 md:my-10 text-lg md:text-2xl font-bold bg-cyan-900 p-3 md:p-4 rounded-2xl'>ম্যাসের একমাত্র নিরপেক্ষ মানুষ <span className='text-xl md:text-3xl text-red-600'> জুবায়ের</span></h1>
+                <div className='flex flex-col md:flex-row justify-evenly items-center font-bold gap-4 md:gap-0'>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>বিএনপি <br />
+                        <span className='mt-2 block'>১৮০ থেকে ১২০</span>
+                    </p>
+                    <p className='text-xl md:text-3xl bg-[#6c6867] items-center p-3 md:p-4 rounded-2xl text-center w-full md:w-auto'>জামাত<br />
+                        <span className='mt-2 block'>৮০ থেকে ৯০</span>
                     </p>
                 </div>
             </div>
 
             </div>
+
+            <div className='mt-10 flex justify-center text-sm font-light'>ALL RIGHT RESERVE TO XUBAEAR</div>
 
         </div>
     );
